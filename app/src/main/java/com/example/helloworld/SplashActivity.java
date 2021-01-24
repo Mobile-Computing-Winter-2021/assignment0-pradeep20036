358bb9292c4483bc976da73891393711d2fd289f
pradeep20036 <pradeep20036@iiitd.ac.in>
2021-01-24 15:12:25 +0530

    Splash Screen is animated

--- app/src/main/java/com/example/helloworld/SplashActivity.java
+++ app/src/main/java/com/example/helloworld/SplashActivity.java
@@ -4,24 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import static java.lang.Thread.sleep;
 
 public class SplashActivity extends AppCompatActivity {
 
+//    variables
+    Animation topAnim,bottomAnim;
+    ImageView image;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
+
+//        Animations
+        topAnim=AnimationUtils.loadAnimation(this,R.anim.image_animation);
+        bottomAnim=AnimationUtils.loadAnimation(this,R.anim.text_animation);
+
+//        Hooks
+        image=findViewById(R.id.imageView);
+        tv=findViewById(R.id.textView2);
+
+        image.setAnimation(topAnim);
+        tv.setAnimation(bottomAnim);
+
 
         new Thread(new Runnable() {
             @Override
             public void run() {
 
                 try {
-                    sleep(3000);
+                    sleep(5000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
